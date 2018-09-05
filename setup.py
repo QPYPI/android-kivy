9ebdd18ca75850aad3b986ef66d7d79e9d88d4c3
river <riverfor@gmail.com>
2018-09-05 03:52:01 -0400

    Commit setup

--- setup.py
+++ setup.py
@@ -10,17 +10,17 @@ setup(name='android',
         Extension(
             'android._android', ['android/_android.c', 'android/_android_jni.c'],
             libraries=[ 'sdl', 'log' ],
-            library_dirs=[ 'libs/' + os.environ['ARCH'] ],
+            #library_dirs=[ 'libs/' + os.environ['ARCH'] ],
             ),
         Extension(
             'android._android_billing', ['android/_android_billing.c', 'android/_android_billing_jni.c'],
             libraries=[ 'log' ],
-            library_dirs=[ 'libs/' + os.environ['ARCH'] ],
+            #library_dirs=[ 'libs/' + os.environ['ARCH'] ],
             ),
         Extension(
             'android._android_sound', ['android/_android_sound.c', 'android/_android_sound_jni.c',],
             libraries=[ 'sdl', 'log' ],
-            library_dirs=[ 'libs/' + os.environ['ARCH'] ],
+            #library_dirs=[ 'libs/' + os.environ['ARCH'] ],
             ),
 
         ]
